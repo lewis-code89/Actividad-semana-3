405cf2dd3c0c0fa62fae6cba343b96d15966cd59
Ailthon17 <ailthonalejandrooconleyva@gmail.com>
2025-09-02 09:37:36 -0500

    2

--- trabajo.cs
+++ trabajo.cs
@@ -2,14 +2,17 @@
 
 Console.WriteLine("Bienvenido");
 Console.WriteLine("Calculare tu promnedio final");
+
 Console.Write("Ingrese su primera nota: ");
 double t1 = Convet.ToDouble(Console.ReadLine());
 Console.WriteLine("Ingrese el peso de la primera nota: ");
 double x = Convert.ToDouble(Console.ReadLine());
+
 Console.Write("Ingrese su segunda nota: ");
 double t2 = Convet.ToDouble(Console.ReadLine());
 Console.WriteLine("Ingrese el peso de su segunda nota: ");
 double y = Convert.ToDouble(Console.ReadLine());
+
 Console.Write("Ingrese su tercera nota: ");
 double t3 = Convet.ToDouble(Console.ReadLine());
 Console.WriteLine("Ingrese el peso de su tercera nota: ");
